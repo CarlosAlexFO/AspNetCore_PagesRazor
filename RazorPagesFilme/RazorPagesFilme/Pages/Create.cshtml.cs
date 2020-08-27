9ebe0cff80d4012a70efb022d5a198588984731f
CarlosAlexFO <carlosoliveira121@gmail.com>
2020-08-27 17:34:39 -0300

    Paginas Web com CRUD utilizando o Razor

---
+++ RazorPagesFilme/RazorPagesFilme/Pages/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesFilme.Data;
+using RazorPagesFilme.Models;
+
+namespace RazorPagesFilme.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesFilme.Data.RazorPagesFilmeContext _context;
+
+        public CreateModel(RazorPagesFilme.Data.RazorPagesFilmeContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Filme Filme { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Filme.Add(Filme);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorPagesFilme/RazorPagesFilme/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesFilme.Data;
+using RazorPagesFilme.Models;
+
+namespace RazorPagesFilme.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesFilme.Data.RazorPagesFilmeContext _context;
+
+        public IndexModel(RazorPagesFilme.Data.RazorPagesFilmeContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Filme> Filme { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Filme = await _context.Filme.ToListAsync();
+        }
+    }
+}
